67639426f383e969bee37632a3a79d007aec396e
arthur06martins <arthur12martins12@gmail.com>
2023-09-04 18:18:31 -0300

    commit inicial

---
+++ src/main/java/CadastroCliente.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class CadastroCliente {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("""
+          ===================================
+            Seja Bem vindo ao Login ADERA!
+            Informe o seu E-mail:""");
+        String email = sc.nextLine();
+
+        System.out.println(""" 
+          Informe o sua Senha:""");
+        String senha = sc.nextLine();
+        System.out.println("===================================");
+
+        if()
+
+    }
+}
