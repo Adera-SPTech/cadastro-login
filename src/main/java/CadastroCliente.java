20bddf3185e4921a61a3d3ded4b64edc4a2793b3
arthur06martins <arthur12martins12@gmail.com>
2023-09-04 19:53:40 -0300

    Atualização para o log

--- src/main/java/CadastroCliente.java
+++ src/main/java/CadastroCliente.java
@@ -4,18 +4,41 @@ public class CadastroCliente {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
+        String senha = null;
+        String email = null;
+        String regex =  "^(.+)@(\\S+)$"; //regex de email @tst.com
+        Boolean acessoNegado = true; //validação de acesso.
+
         System.out.println("""
-          ===================================
-            Seja Bem vindo ao Login ADERA!
+            ===================================
+            Seja Bem vindo ao Login ADERA!""");
+
+      while(acessoNegado){
+          System.out.println("""
             Informe o seu E-mail:""");
-        String email = sc.nextLine();
+          email = sc.nextLine(); //Solicitando o email
 
-        System.out.println(""" 
-          Informe o sua Senha:""");
-        String senha = sc.nextLine();
-        System.out.println("===================================");
+          //validando o email para que não seja null e que tenha a regex de email
+          if(email.matches(regex) && email != null){
+              System.out.println("Informe sua Senha:");
+              senha = sc.nextLine();
+              System.out.println("===================================");
 
-        if()
+              //validando para que a senha não seja null
+              if(senha != null ){
+                  System.out.println("Bem vindo ao ambiente ADERA!");
+                  acessoNegado = false;
+              }
+              else{
+                  System.out.print("Senha invalida!\n");
+              }
+
+
+          }
+          else{
+              System.out.print("E-mail invalido!\n\n");
+          }
+      }
 
     }
 }
